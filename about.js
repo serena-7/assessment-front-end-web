60ca7466141b42eea6e0b78f38466e0c3d9c6678
Serena Sorensen <serena.sorensen7@gmail.com>
2022-04-08 10:24:54 -0600

    assessment problems finished

--- about.js
+++ about.js
@@ -4,11 +4,16 @@ console.log("hello world");
 
 function handleSubmit(evt) {
 	evt.preventDefault();
-	
-	console.log('form submit');
+	alert("Submitted Successfully")
+	// console.log('form submit');
 }
 
+function yourCool(evt) {
+	alert("You're Cool!")
+}
 
 let form = document.querySelector('form#contact');
+let img = document.querySelector('img');
 
 form.addEventListener('submit', handleSubmit);
+img.addEventListener('mouseover', yourCool);
---
+++ profile.js
@@ -0,0 +1,9 @@
+let colorBtn = document.querySelector("#color");
+let placeBtn = document.querySelector("#place");
+let ritualBtn = document.querySelector("#ritual");
+
+colorBtn.addEventListener('click', () => alert("My favorite color is teal"));
+
+placeBtn.addEventListener('click', () => alert("My favorite place is home"));
+
+ritualBtn.addEventListener('click', () => alert("My favorite ritual is eating a snack before bed"))
